b7012a6fe6ff41f6af42184094eecb556d1917dc
Roberto Sanchez <rsanchez92@live.com>
2019-07-23 15:56:54 -0700

    Completed Insert Snippet

--- server/index.js
+++ server/index.js
@@ -2,12 +2,20 @@ const controller = require('./models/Snippet.model.js');
 
 async function testModels() {
   try{
+    // Testing Select
+    /*********************************************
     const snippets = await controller.select({id: '1'});
-    console.log(snippets)
-  }catch(err){
-    console.log(`\tOOPSIE WOOPSIE: ${err}`)
+    console.log(snippets);
+    //*////////////////////////////////////////////////
 
-  }
+    // Testing Insert
+    //*********************************************
+    const snippet = await controller.insert({author: "Robert",code: "console.log(Hello World);",description: "It displays hello world", title: "adsf", language: "javascript"}); 
+    console.log(snippet);
+    //*////////////////////////////////////////////////
     
+  }catch(err){
+    console.error(`\t(!) An error has occurred: ${err}`);
+  }
 }
 testModels();
--- server/models/Snippet.model.js
+++ server/models/Snippet.model.js
@@ -1,22 +1,62 @@
 const fs = require('fs').promises;
 const path = require('path');
-/* Create */
-exports.create = () =>{
+const shortid = require('shortid');
+/**
+ * @typedef {Object} Snippet
+ * @property {string} id  
+ * @property {string} author
+ * @property {string} code 
+ * @property {string} title  
+ * @property {string} description
+ * @property {string} language
+ * @property {string[]} comments
+ * @property {number} favorites
+ */
+/**
+ * Inserts a snippet into the database
+ * @param {Snippet} newSnippet the data to create the snippet with
+ * @returns {Promise<Snippet>} the created snippet
+ */
+exports.insert = async ({author, code, title, description, language}) => {
+  try{
+    if(!author || !code || !title || !description || !language){
+      throw new Error("Missing Parameters when inserting into database.");
+    }
+    const dbpath = path.join(__dirname,'..','db','snippets.json');
+    const snippets = JSON.parse(await fs.readFile(dbpath));
+    //generate default data
+    snippets.push({
+      id: shortid.generate(),
+      author,
+      title,
+      description,
+      language,
+      comments: [],
+      favorites: 0
+    });
+    await fs.writeFile(dbpath,JSON.stringify(snippets));
+    return snippets[snippets.length -1];
+
+  }catch(err){
+    throw err;
+  }
   
 }
-/* Read */
+/**
+ * Selects snippets from database
+ * @param {object{}} [query] Optional if you want a specific records.
+ * @returns {Promise<Snippet[]>} Ann array of objects
+ */
 exports.select = async (query = {}) => {
   try {
-    const dbpath = path.join(__dirname,'..','db','snipfpets.json');
+    const dbpath = path.join(__dirname,'..','db','snippets.json');
     const snippets = JSON.parse(await fs.readFile(dbpath));
-    // Filter Snipets with a query
     const filtered = snippets.filter(snippet =>
       Object.keys(query).every(key => query[key] === snippet[key])
     );
     return filtered;
   }
   catch(err){
-    console.log(`\t (!) An error has occured: ${err}.`);
     throw err;
   }
 }
